63fa7b26b4d6e9e25dd82dfc0a0177b70bbf47a3
roymustang <mustangroy3000@gmail.com>
2026-01-13 10:36:17 +0530

    Phase 4 Step 1.1: add username field to User schema

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,6 +1,14 @@
 const mongoose=require("mongoose");
 
 const userSchema=new mongoose.Schema({
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true
+    },
+    
     name:{
         type: String,
         required: true,
