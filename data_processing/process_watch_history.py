4e5f7790a67049ee85fe8b871140f540ef29dc89
roymustang <mustangroy3000@gmail.com>
2026-01-13 14:36:58 +0530

    Phase 4 Step 3: Improve similarity logic using  time weighting

--- data_processing/process_watch_history.py
+++ data_processing/process_watch_history.py
@@ -1,6 +1,8 @@
 import pandas as pd
 from datetime import datetime, timedelta
+from datetime import timezone
 
+import math
 import json
 import sys
 
@@ -36,8 +38,23 @@ def filter_recent_history(df, months=3):
     return df
 
 def compute_frequencies(df):
-    channel_freq= (df.groupby("channel").size().sort_values(ascending=False).to_dict())
-    category_freq= (df.groupby("category").size().sort_values(ascending=False).to_dict())
+    channel_freq={}
+    category_freq={}
+
+    for _, row in df.iterrows():
+        channel = row["channel"]
+        category=row["category"]
+        watched_at=row["watched_at"]
+
+        if pd.isna(watched_at):
+            continue
+        decay_weight = compute_decay_weight(watched_at)
+
+        #If the channel was seen before → add to its existing score
+        #If the channel is new → start from 0 and add
+        channel_freq[channel]=channel_freq.get(channel, 0)+decay_weight
+
+        category_freq[category] = category_freq.get(category, 0) + decay_weight
     
     return channel_freq, category_freq
 
@@ -60,8 +77,12 @@ def process_watch_history(csv_path):
         "categories": category_freq
         }
 
+def compute_decay_weight(watched_at, lambda_=0.05):
+    now = datetime.now(timezone.utc)
+    days_ago = (now - watched_at).days
+    return math.exp(-lambda_*days_ago)
+
 # When Node calls Python, Python must use sys.argv, not hardcoded filenames
 csv_path = sys.argv[1]
 result=process_watch_history(csv_path)
 print(json.dumps(result))
-    
